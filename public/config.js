17af3ee008209f31f6f9275799c44ed5ff26a0e7
David Blane <david.blane@digicatapult.org.uk>
2022-03-01 17:26:09 +0000

    Fix prettier

--- public/config.js
+++ public/config.js
@@ -1,14 +1,14 @@
 let config = {
-  "authDomain": "inteli.eu.auth0.com",
-  "clientID": "A5PgyCfBMIOhHQEIyImtqLGuD1VJ9VgP",
-  "custAuthClientID": "BvJaBbxOce4Pwi5PZpjBTStvNWwzugPd",
-  "t1AuthClientID": "6NfUcA48JdhKXYCc9yGTwM2m1gzcjOTU",
-  "authAudience": "inteli-dev",
-  "apiHost": "localhost",
-  "apiPort": "3000",
-  "substrateHost": "localhost",
-  "substratePort": "9944",
-  "vitalamDemoPersona": "cust",
+  'authDomain': 'inteli.eu.auth0.com',
+  'clientID': 'A5PgyCfBMIOhHQEIyImtqLGuD1VJ9VgP',
+  'custAuthClientID': 'BvJaBbxOce4Pwi5PZpjBTStvNWwzugPd',
+  't1AuthClientID': '6NfUcA48JdhKXYCc9yGTwM2m1gzcjOTU',
+  'authAudience': 'inteli-dev',
+  'apiHost': 'localhost',
+  'apiPort': '3000',
+  'substrateHost': 'localhost',
+  'substratePort': '9944',
+  'vitalamDemoPersona': 'cust',
 }
 
 if (!Window.config) {
