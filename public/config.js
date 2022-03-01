d718ff67391fb50186107889923e06be22046069
Daniel Cooper <danielcooperxyz@gmail.com>
2022-03-01 15:22:42 +0000

    update config.js to create Window variable config

--- public/config.js
+++ public/config.js
@@ -1,4 +1,15 @@
-export const config=
-{
-  "foo": "bar"
-};
+let config = {
+  persona: 'cust',
+}
+
+if (!Window.config) {
+  Window.config = {}
+}
+
+if (!Window.config.inteli) {
+  Window.config.inteli = {}
+}
+
+for (const [key, value] of Object.entries(config)) {
+  Window.config.inteli[key] = value
+}
