85cc07a8b72c51eec482d8b7852af055d70853ef
David Blane <david.blane@digicatapult.org.uk>
2022-03-01 10:02:11 +0000

    blank gh-pages branch

--- src/utils/metadataTypes.js
+++
@@ -1,8 +0,0 @@
-const metadataTypes = {
-  file: 'FILE',
-  literal: 'LITERAL',
-  tokenId: 'TOKEN_ID',
-  none: 'NONE',
-}
-
-export default metadataTypes
--- src/utils/tokenTypes.js
+++
@@ -1,7 +0,0 @@
-const tokenTypes = {
-  powderTest: 'POWDER_TEST',
-  powder: 'POWDER',
-  order: 'ORDER',
-}
-
-export default tokenTypes
