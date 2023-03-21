b7ba44aa6db2a3b22e8b6a2b78327f6f72546ebd
Matthew Dean <matthew.dean@digicatapult.org.uk>
2023-03-21 12:31:21 +0000

    Fix yq

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -4,10 +4,28 @@
 set -e
 
 function check_versions_consistent () {
+<<<<<<< Updated upstream
   local PACKAGE_VERSION=$(yq eval '.version' ./package.json)
   local PACKAGE_LOCK_VERSION=$(yq eval '.version' ./package-lock.json)
+<<<<<<< Updated upstream
 
   if [ "$PACKAGE_VERSION" != "$PACKAGE_LOCK_VERSION" ]; then
+=======
+  local HELM_VALUES_TAG_VERSION=$(yq eval '.image.tag' ./helm/inteli-demo/values.yaml)
+  local HELM_CHART_VERSION=$(yq eval '.version' ./helm/inteli-demo/Chart.yaml)
+  local HELM_CHART_APP_VERSION=$(yq eval '.appVersion' ./helm/inteli-demo/Chart.yaml)
+  
+  if [ "$PACKAGE_VERSION" != "$PACKAGE_LOCK_VERSION" ] ||
+     [ "v$PACKAGE_VERSION" != "$HELM_VALUES_TAG_VERSION" ] ||
+     [ "$PACKAGE_VERSION" != "$HELM_CHART_VERSION" ] ||
+     [ "$PACKAGE_VERSION" != "$HELM_CHART_APP_VERSION" ]; then
+=======
+  local PACKAGE_VERSION=$(yq eval -o y '.version' ./package.json)
+  local PACKAGE_LOCK_VERSION=$(yq eval -o y '.version' ./package-lock.json)
+
+  if [ "$PACKAGE_VERSION" != "$PACKAGE_LOCK_VERSION" ]; then
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     echo "Inconsistent versions detected"
     echo "PACKAGE_VERSION: $PACKAGE_VERSION"
     echo "PACKAGE_LOCK_VERSION: $PACKAGE_LOCK_VERSION"
@@ -40,7 +58,7 @@ function check_version_greater () {
 # Get published git tags that match semver regex with a "v" prefix then remove the "v" character
 PUBLISHED_VERSIONS=$(git tag | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+\(\-[a-zA-Z-]\+\(\.[0-9]\+\)*\)\{0,1\}$" | sed 's/^v\(.*\)$/\1/')
 # Get the current version from node Cargo.toml
-CURRENT_VERSION=$(yq eval '.version' ./package.json)
+CURRENT_VERSION=$(yq eval -o y '.version' ./package.json)
 
 if check_version_greater "$CURRENT_VERSION" "$PUBLISHED_VERSIONS"; then
   echo "##[set-output name=VERSION;]v$CURRENT_VERSION"
